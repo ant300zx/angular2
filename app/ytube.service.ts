b3fbfac622012f65e7fff96160d789a72c476963
Anthony Cull <anthony@concreteplatform.com>
2016-08-15 18:29:03 +0100

    Add the youTube service

---
+++ app/ytube.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Ytube} from './video';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+
+export class YtubeService {
+
+  constructor (private http: Http) {}
+
+  private ytubeURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=tutsplus&type=video&key={AIzaSyBibEh8R8x5sBa2pEZMAKMPbw32-s-KM3g}'
+
+  getVideos() {
+    return this.http.get(this.ytubeURL)
+    .map(res => <Ytube[]> res.json().items)
+    .do(data => console.log(data))
+    .catch(this.errorHandler);
+  }
+
+  private errorHandler (error: Response) {
+    console.log(error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
+}
